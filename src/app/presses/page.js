290be4ae4168a69d06f8a34d362aa8df28c343e7
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-23 20:21:58 +0200

    Modificate Cards

--- src/app/presses/page.js
+++ src/app/presses/page.js
@@ -10,41 +10,49 @@ export default function Presses() {
           {/* container start */}
 
           <Card
+            description=" “Ristrutturazione di un appartamento”, articolo pubblicato nel sito online “TUBES”."
+            imageSourceLeft="/Pubblicazioni/TUBES.JPG"
+            imageSourceRight="/Pubblicazioni/TUBES02.JPG"
+            source="https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/"
+             date="2022"
+          >
+            {" "}
+          </Card>
+
+          <Card
+            description=" “15 motivi (….)”, articolo pubblicato nella rivista online “HOMIFY”
+            https://www.homify.it/librodelleidee/7792268/15-motivi-per-scegliere-il-cocciopesto-e-i-laterizi-per-i-rivestimenti "
+            imageSourceLeft="/IMG_25.JPG"
+            imageSourceRight="/IMG_25.JPG"
+            date="Febbraio 2021"
+          >
+            {" "}
+          </Card>
+
+          <Card
+            description=" Recupero dei sottotetti(….)”, articolo pubblicato nella rivista online “HOMIFY”, 
+            https://www.homify.it/librodelleidee/6101558/recupero-dei-sottotetti-misure-e-normative-per-il-piemonte "
+            imageSourceLeft="/IMG_25.JPG"
+            imageSourceRight="/IMG_25.JPG"
+            date="Novembre 2018"
+          >
+            {" "}
+          </Card>
+
+
+          <Card
             description="rivista di architettura"
-            imageSource="/IMG_25.JPG"
+            imageSourceLeft="/IMG_25.JPG"
+            imageSourceRight="/IMG_25.JPG"
             date="01/04/2018"
           >
             {" "}
           </Card>
 
           <Card
-            description="https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/"
-            imageSource="/tubes.jpg"
-            date="2013"
-          >
-            {" "}
-          </Card>
-
-          <Card
             description="rivista di architettura"
-            imageSource="/IMG_25.JPG"
-            date="01/04/2018"
-          >
-            {" "}
-          </Card>
-
-
-          <Card
-            description="rivista di architettura"
-            imageSource="/IMG_25.JPG"
-            date="01/04/2018"
-          >
-            {" "}
-          </Card>
-
-          <Card
-            description="rivista di architettura"
-            imageSource="/IMG_25.JPG"
+            imageSourceLeft="/IMG_25.JPG"
+            imageSourceRight="/IMG_25.JPG"
             date="01/04/2018"
           >
             {" "}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-export default function Card({ description, imageSource, date }) {
+export default function Card({ description, imageSourceLeft, imageSourceRight, source, date }) {
   return (
     <>
       <div class="rounded overflow-hidden shadow-lg ">
       <div style={{ display: "flex" }}>
-        <img src={imageSource} alt="contacts" className="w-1/2" />
-        <img src={imageSource} alt="contacts" className="w-1/2"/>
+        <img src={imageSourceLeft} alt="" className="w-1/2" />
+        <img src={imageSourceRight} alt="" className="w-1/2"/>
         </div>
         <div class="px-6 py-4 ">
           <div class="font-bold text-lg mb-2 break-words">{description}</div>
+          <div class="text-md mb-2 break-words">{source}</div>
           <p class="text-gray-700 text-base">{date}</p>
         </div>
       </div>
