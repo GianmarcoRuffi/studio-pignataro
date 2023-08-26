11140a021a376fd8bb5324e569ee3c2b2e69777b
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-26 12:35:45 +0200

    ingrandite immagini bio e contacts

--- src/app/bio/page.js
+++ src/app/bio/page.js
@@ -5,190 +5,179 @@ import styles from "../styles/Bio.module.css";
 export default function Bio() {
   return (
     <div className="bio-container flex bg-gray-100 max-lg:flex-col">
-      
-        <div className="image-container lg:w-1/2 lg:mr-4">
-          <img src="/Bio.jpg" alt="bio" className="max-w-full h-auto"/>
-        </div>
-
-        <div className="bio-box lg:w-1/2 text-justify">
-          <h1>Gianluca Pignataro</h1>
-          <h2>Arch. Gianluca Pignataro</h2>
-          <p>
-            Nato a Bologna e diplomato in studi scientifici, laureato a Firenze
-            ho intrapreso la professione a Cagliari dove ho da subito affrontato
-            tematiche di restauro e di recupero dell’edilizia storica e di
-            progettazione architettonica. La pratica professionale svolta in 30
-            anni mi ha specializzato nel campo del restauro e recupero
-            dell’edilizia storica grazie ai molteplici incarichi tra cui il
-            recupero di sette edifici nel centro storico di Baunei (OG) per la
-            Stams s.r.l. società del gruppo Cualbu. Nel campo della
-            progettazione architettonica ho operato in Italia per clienti come
-            Findomestic Banca, imprese di costruzione, cooperative e privati e
-            grazie alla doppia cittadinanza italiana e statunitense, ho
-            realizzato a Puerto Rico (U.S.A.) cinque negozi in centri
-            commerciali ed una villa.
-          </p>
-          <h2>
-            
-            Esperienze professionali
-          </h2>
-          <h3>
-            Studio Dott. Arch. Gianluca Pignataro – Cagliari, da Gennaio 2004 ad
-            oggi
-          </h3>
-          <h4>Fondatore – Titolare. </h4>
-
-          <p>
-          Architetto progettista di interventi
-            nell’edilizia residenziale moderna e nel recupero di edifici in
-            centro storico.
-          </p>
-          <ul>
-            <li>Casa Contu: Ristrutturazione con Interior design a Cagliari</li>
-            <li>
-              Responsabile unico del team di Architetti ed Ingegneri per le
-              verifiche edilizie ed urbanistiche degli edifici aderenti alla
-              normativa del Superbonus 110%, per il Coach Manager Regionale di
-              “HARLEY & DIKKINSON CONSULTING S.r.l.” Ing. Giuseppe De Bonis.
-            </li>
-            <li>
-              Casa Reinhold-Porcu: Ristrutturazione con Interior design nel
-              centro storico di Cagliari
-            </li>
-            <li>
-              Palazzo Asquer: Messa in sicurezza strutturale con restauro di
-              resti romani nel seminterrato del Palazzo storico Asquer,
-              vincolato dalla Soprintendenza Ministeriale (SABAP) nel centro
-              storico di Cagliari quartiere Castello
-            </li>
-            <li>
-              Agenzia pubblicitaria Glisbo: Stand modulare per la “Regione
-              Autonoma della Sardegna” <a href="https://glisbo.com/" target="_blank">www.glisbo.com</a>
-            </li>
-            <li>
-              Casa Corda: Ristrutturazione con Interior design a Cagliari
-              www.luigicorda.com (lavoro pubblicato)
-            </li>
-            <li>
-              Perito estimatore per la “Banca di Credito Cooperativo di
-              Cagliari”
-            </li>
-            <li>
-              Casa Guicciardi: Ristrutturazione con Interior design a Cagliari{" "}
-              <a
-                href="https://www.homify.it/librodelleidee/778802/un-appartamento-moderno-rivoluzionario"
-                target="_blank"
-              >
-                https://www.homify.it/librodelleidee/778802/un-appartamento-moderno-rivoluzionario
-              </a>
-              {" "}
-            
-            </li>
-
-            <li>  <a
-                href="https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/"
-                target="_blank"
-              >
-                https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/
-              </a>{" "}
-              (lavoro pubblicato)</li>
-            <li>
-              Nemesis Data s.r.l.: Ampliamento di una unità residenziale a Porto
-              Cervo, Arzachena (OT)
-            </li>
-            <li>
-              Casa Piras: Ristrutturazione con Interior design nel centro
-              storico di Cagliari, quartiere Villanova (lavoro pubblicato)
-            </li>
-            <li>
-              Casa Barranu: Ristrutturazione di una palazzina nel centro storico
-              di Baunei (OG)
-            </li>
-            <li>
-              Casa D’Alessio: Ristrutturazione con Interior design nel palazzo
-              storico Zedda-Pirasa Cagliari
-            </li>
-            <li>
-              Casa Vincis: Progettazione e d.l. di una palazzina residenziale a
-              Cagliari
-            </li>
-            <li>
-              Stams s.r.l. (Gruppo Cualbu): Progettazione e direzione artistica
-              del recupero di sette edifici nel centro storico di Baunei (OG){" "}
-              <a href="http://www.dommoso.it/en/index.html" target="_blank">
-                http://www.dommoso.it/en/index.html
-              </a>
-            </li>
-            <li>
-              Tangex Corp.: Progettazione di una villa a Ocean Park San Juan,
-              Puerto Rico (U.S.A.)
-            </li>
-          </ul>
-          {" "}
-          <h3>
-            Archlabo - Pignataro Vandermeulen Architetti – Cagliari, da Gennaio
-            1993 a Dicembre 2003
-          </h3>
-          <h4>Co-fondatore.</h4>
-          <p>
-            Architetto progettista principale, dalla ideazione
-            sino alla realizzazione in cantiere di progetti residenziali,
-            commerciali e di design di arredi; componente di un team composto da
-            due architetti ed un ingegnere. Responsabile dei rapporti con
-            committenza e imprese.
-          </p>
-          <ul>
-            <li>
-              Tangex Corp.: Progettazione di cinque negozi in centri commerciali
-              a Puerto Rico (U.S.A.)
-            </li>
-            <li>
-              Cooperativa edilizia Urbus: progettazione di un edificio
-              residenziale a Cagliari
-            </li>
-            <li>
-              Findomestic Banca: progettazione e d.l. delle “Sedi clienti/uffici
-              territorio” a Cagliari e Sassari
-            </li>
-            <li>
-              Discoteca Pandemonium: progettazione e d.l., Quartu S.E. (CA)
-            </li>
-            <li>
-              Impresa Sarritzu: Progettazione di un Albergo Residence per 120
-              studenti a Cagliari
-            </li>
-            <li>
-              Edificio Commendatore: Progettazione, Interior design e d.l. di
-              una palazzina residenziale a Cagliari
-            </li>
-            <li>
-              Casa Cugusi: ristrutturazione di un appartamento nel centro
-              storico di Cagliari, quartiere Castello (lavoro pubblicato)
-            </li>
-          </ul>
-          {" "}
-          <h3>
-            Studio Dott. Arch. Giovanni Maria Campus – Cagliari, da Genn. 1991 a
-            Dic. 1993
-          </h3>
-          <h4>Progettista</h4>
-          <p>
-            Progettista unico o in partecipazione con altri architetti dalla
-            fase ideativa sino alla realizzazione cantieristica dei seguenti
-            progetti:
-          </p>
-          <ul>
-            <li>Restauro di Palazzo Aresu, nel centro storico di Cagliari</li>
-            <li>
-              Progettazione della “Casa di Riposo per anziani” ad Arzachena (OT)
-            </li>
-            <li>
-              Progettazione del “Hotel S.Efis” a Pula, località Nora (Cagliari)
-            </li>
-            <li>Restauro della chiesa della SS. Annunziata a Cagliari</li>
-          </ul>
-        </div>
+      <div className="image-container lg:w-2/3 lg:mr-4">
+        <img src="/Bio.jpg" alt="bio" className="max-w-full h-auto" />
       </div>
-    
+
+      <div className="bio-box lg:w-1/3 text-justify">
+        <h1>Gianluca Pignataro</h1>
+        <h2>Arch. Gianluca Pignataro</h2>
+        <p>
+          Nato a Bologna e diplomato in studi scientifici, laureato a Firenze ho
+          intrapreso la professione a Cagliari dove ho da subito affrontato
+          tematiche di restauro e di recupero dell’edilizia storica e di
+          progettazione architettonica. La pratica professionale svolta in 30
+          anni mi ha specializzato nel campo del restauro e recupero
+          dell’edilizia storica grazie ai molteplici incarichi tra cui il
+          recupero di sette edifici nel centro storico di Baunei (OG) per la
+          Stams s.r.l. società del gruppo Cualbu. Nel campo della progettazione
+          architettonica ho operato in Italia per clienti come Findomestic
+          Banca, imprese di costruzione, cooperative e privati e grazie alla
+          doppia cittadinanza italiana e statunitense, ho realizzato a Puerto
+          Rico (U.S.A.) cinque negozi in centri commerciali ed una villa.
+        </p>
+        <h2>Esperienze professionali</h2>
+        <h3>
+          Studio Dott. Arch. Gianluca Pignataro – Cagliari, da Gennaio 2004 ad
+          oggi
+        </h3>
+        <h4>Fondatore – Titolare. </h4>
+        <p>
+          Architetto progettista di interventi nell’edilizia residenziale
+          moderna e nel recupero di edifici in centro storico.
+        </p>
+        <ul>
+          <li>Casa Contu: Ristrutturazione con Interior design a Cagliari</li>
+          <li>
+            Responsabile unico del team di Architetti ed Ingegneri per le
+            verifiche edilizie ed urbanistiche degli edifici aderenti alla
+            normativa del Superbonus 110%, per il Coach Manager Regionale di
+            “HARLEY & DIKKINSON CONSULTING S.r.l.” Ing. Giuseppe De Bonis.
+          </li>
+          <li>
+            Casa Reinhold-Porcu: Ristrutturazione con Interior design nel centro
+            storico di Cagliari
+          </li>
+          <li>
+            Palazzo Asquer: Messa in sicurezza strutturale con restauro di resti
+            romani nel seminterrato del Palazzo storico Asquer, vincolato dalla
+            Soprintendenza Ministeriale (SABAP) nel centro storico di Cagliari
+            quartiere Castello
+          </li>
+          <li>
+            Agenzia pubblicitaria Glisbo: Stand modulare per la “Regione
+            Autonoma della Sardegna”{" "}
+            <a href="https://glisbo.com/" target="_blank">
+              www.glisbo.com
+            </a>
+          </li>
+          <li>
+            Casa Corda: Ristrutturazione con Interior design a Cagliari
+            www.luigicorda.com (lavoro pubblicato)
+          </li>
+          <li>
+            Perito estimatore per la “Banca di Credito Cooperativo di Cagliari”
+          </li>
+          <li>
+            Casa Guicciardi: Ristrutturazione con Interior design a Cagliari{" "}
+            <a
+              href="https://www.homify.it/librodelleidee/778802/un-appartamento-moderno-rivoluzionario"
+              target="_blank"
+            >
+              https://www.homify.it/librodelleidee/778802/un-appartamento-moderno-rivoluzionario
+            </a>{" "}
+          </li>
+
+          <li>
+            {" "}
+            <a
+              href="https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/"
+              target="_blank"
+            >
+              https://www.tubesradiatori.com/it/progetti/ristrutturazione-di-un-appartamento/
+            </a>{" "}
+            (lavoro pubblicato)
+          </li>
+          <li>
+            Nemesis Data s.r.l.: Ampliamento di una unità residenziale a Porto
+            Cervo, Arzachena (OT)
+          </li>
+          <li>
+            Casa Piras: Ristrutturazione con Interior design nel centro storico
+            di Cagliari, quartiere Villanova (lavoro pubblicato)
+          </li>
+          <li>
+            Casa Barranu: Ristrutturazione di una palazzina nel centro storico
+            di Baunei (OG)
+          </li>
+          <li>
+            Casa D’Alessio: Ristrutturazione con Interior design nel palazzo
+            storico Zedda-Pirasa Cagliari
+          </li>
+          <li>
+            Casa Vincis: Progettazione e d.l. di una palazzina residenziale a
+            Cagliari
+          </li>
+          <li>
+            Stams s.r.l. (Gruppo Cualbu): Progettazione e direzione artistica
+            del recupero di sette edifici nel centro storico di Baunei (OG){" "}
+            <a href="http://www.dommoso.it/en/index.html" target="_blank">
+              http://www.dommoso.it/en/index.html
+            </a>
+          </li>
+          <li>
+            Tangex Corp.: Progettazione di una villa a Ocean Park San Juan,
+            Puerto Rico (U.S.A.)
+          </li>
+        </ul>{" "}
+        <h3>
+          Archlabo - Pignataro Vandermeulen Architetti – Cagliari, da Gennaio
+          1993 a Dicembre 2003
+        </h3>
+        <h4>Co-fondatore.</h4>
+        <p>
+          Architetto progettista principale, dalla ideazione sino alla
+          realizzazione in cantiere di progetti residenziali, commerciali e di
+          design di arredi; componente di un team composto da due architetti ed
+          un ingegnere. Responsabile dei rapporti con committenza e imprese.
+        </p>
+        <ul>
+          <li>
+            Tangex Corp.: Progettazione di cinque negozi in centri commerciali a
+            Puerto Rico (U.S.A.)
+          </li>
+          <li>
+            Cooperativa edilizia Urbus: progettazione di un edificio
+            residenziale a Cagliari
+          </li>
+          <li>
+            Findomestic Banca: progettazione e d.l. delle “Sedi clienti/uffici
+            territorio” a Cagliari e Sassari
+          </li>
+          <li>Discoteca Pandemonium: progettazione e d.l., Quartu S.E. (CA)</li>
+          <li>
+            Impresa Sarritzu: Progettazione di un Albergo Residence per 120
+            studenti a Cagliari
+          </li>
+          <li>
+            Edificio Commendatore: Progettazione, Interior design e d.l. di una
+            palazzina residenziale a Cagliari
+          </li>
+          <li>
+            Casa Cugusi: ristrutturazione di un appartamento nel centro storico
+            di Cagliari, quartiere Castello (lavoro pubblicato)
+          </li>
+        </ul>{" "}
+        <h3>
+          Studio Dott. Arch. Giovanni Maria Campus – Cagliari, da Genn. 1991 a
+          Dic. 1993
+        </h3>
+        <h4>Progettista</h4>
+        <p>
+          Progettista unico o in partecipazione con altri architetti dalla fase
+          ideativa sino alla realizzazione cantieristica dei seguenti progetti:
+        </p>
+        <ul>
+          <li>Restauro di Palazzo Aresu, nel centro storico di Cagliari</li>
+          <li>
+            Progettazione della “Casa di Riposo per anziani” ad Arzachena (OT)
+          </li>
+          <li>
+            Progettazione del “Hotel S.Efis” a Pula, località Nora (Cagliari)
+          </li>
+          <li>Restauro della chiesa della SS. Annunziata a Cagliari</li>
+        </ul>
+      </div>
+    </div>
   );
 }
--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -4,26 +4,24 @@ import Image from "next/image";
 export default function Contacts() {
   return (
     <div className="contacts-container flex justify-center items-center bg-gray-100 max-lg:flex-col">
-      
-        <div className="image-container lg:w-1/2 lg:pr-4 ">
-          <img src="/Studio.jpg " alt="studio" className="max-w-full" />
-        </div>
-
-        <div className="contacts-box py-8  lg:w-1/2 flex items-center ">
-          <ul className="contacts text-sm ">
-            <li className="p-4">
-              <strong>Studio:</strong> Via Ada Negri 12, 09127 Cagliari
-            </li>
-            <li className="p-4">
-              <strong>Email:</strong>{" "}
-              <a href="mailto:glpignataro@yahoo.it">glpignataro@yahoo.it</a>
-            </li>
-            <li className="p-4">
-              <strong>Telefono:</strong> 0703517306 / Cell. 3485189797
-            </li>
-          </ul>
-        </div>
+      <div className="image-container lg:w-2/3 lg:pr-4 ">
+        <img src="/Studio.jpg " alt="studio" className="max-w-full" />
       </div>
-    
+
+      <div className="contacts-box py-8  lg:w-1/3 flex items-center ">
+        <ul className="contacts text-sm ">
+          <li className="p-4">
+            <strong>Studio:</strong> Via Ada Negri 12, 09127 Cagliari
+          </li>
+          <li className="p-4">
+            <strong>Email:</strong>{" "}
+            <a href="mailto:glpignataro@yahoo.it">glpignataro@yahoo.it</a>
+          </li>
+          <li className="p-4">
+            <strong>Telefono:</strong> 0703517306 / Cell. 3485189797
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
