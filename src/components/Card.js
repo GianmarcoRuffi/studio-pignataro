ab054f9ad8a2cfc75ea93537a11535e37c233506
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-29 00:34:53 +0200

    prova modifica caratteri

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,12 +7,14 @@ export default function Card({ description, imageSource, source, date }) {
         <div style={{ display: "flex" }}>
           <img src={imageSource} alt="" />
         </div>
-        <div className="px-6 py-4 ">
-          <div className="font-bold text-lg mb-2 break-words">
+        <div className="px-6 p-4 ">
+          <div className="font-bold text-lg mb-2 break-words sm:max-md:text-sm">
             {description}
           </div>
-          <div className="text-md mb-2 break-words card-link">{source}</div>
-          <p className="text-gray-700 text-base">{date}</p>
+          <div className="text-md mb-2 break-words card-link sm:max-md:text-sm">
+            {source}
+          </div>
+          <p className="text-gray-700 text-base sm:max-md:text-sm">{date}</p>
         </div>
       </div>
     </>
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,12 +5,16 @@ export default function ProjectCard({ name, imageSource, description }) {
     <>
       <div className="rounded overflow-hidden shadow-lg">
         <div className="object-contain">
-          <img src={imageSource} alt="" />
+          <img src={imageSource} alt="" className="" />
         </div>
 
-        <div className="px-6 py-4 ">
-          <div className="font-bold text-lg mb-2 break-words">{name}</div>
-          <p className="text-gray-700 text-base">{description}</p>
+        <div className="px-6 py-4 sm:max-md:text-sm ">
+          <div className="font-bold text-lg mb-2 break-words sm:max-md:text-sm">
+            {name}
+          </div>
+          <p className="text-gray-700 text-base sm:max-md:text-sm">
+            {description}
+          </p>
         </div>
       </div>
     </>
