1e609e00588dfa0ced5542e6c371a76734fbd219
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-30 16:56:06 +0200

    prova flex col

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,7 +12,7 @@ export default function Gallery({
 }) {
   return (
     <div className="flex-col justify-center bg-gray-100">
-      <div className="mb-20">
+      <div className="mb-20 flex flex-col">
         <h1 className="text-3xl font-semibold py-4">{galleryTitle}</h1>
         <p className="text-gray-500">{galleryDescription}</p>
         <p className="text-gray-500 gallery-link text-sm md:text-base">
