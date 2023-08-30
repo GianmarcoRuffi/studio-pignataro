ec26e62163a757cfd7e471e2ec7eea47b74541ce
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-30 17:32:46 +0200

    truncate

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,13 +12,13 @@ export default function Gallery({
 }) {
   return (
     <div className="flex-col justify-center bg-gray-100">
-      <div className="mb-20 flex flex-col align-center ml-2 md:ml-0 w-2/3">
+      <div className="mb-20 flex flex-col align-center ml-2 md:ml-0">
         <h1 className="text-3xl font-semibold py-4">{galleryTitle}</h1>
         <p className="text-gray-500">{galleryDescription}</p>
-        <p className="text-gray-500 gallery-link text-sm md:text-base">
+        <p className="text-gray-500 gallery-link text-sm md:text-base truncate">
           {galleryLink}
         </p>
-        <p className="text-gray-500 gallery-link text-sm md:text-base">
+        <p className="text-gray-500 gallery-link text-sm md:text-base truncate">
           {galleryLinkTwo}
         </p>
         <span className="text-gray-500  text-xs">{imgCredits}</span>
