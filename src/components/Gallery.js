c3b1ec2c9fcc21580583de476a356b33796de731
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-08-30 16:30:37 +0200

    prova fix 1

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,8 +12,8 @@ export default function Gallery({
 }) {
   return (
     <div className="flex justify-center bg-gray-100">
-      <div className="flex flex-col ">
-        <div className="mb-20 flex-wrap gap-8 bg-gray-100 justify-center">
+      <div className="flex flex-wrap gap-8 bg-gray-100 justify-center">
+        <div className="mb-20">
           <h1 className="text-3xl font-semibold py-4">{galleryTitle}</h1>
           <p className="text-gray-500">{galleryDescription}</p>
           <p className="text-gray-500 gallery-link">{galleryLink}</p>
@@ -21,23 +21,22 @@ export default function Gallery({
           <span className="text-gray-500  text-xs">{imgCredits}</span>
         </div>
 
-        <div className="flex flex-wrap gap-8 bg-gray-100 justify-center">
-          {images.map((image, index) => (
-            <div className="border py-4 border-none" key={index}>
-              <Image
-                src={image}
-                alt={`Image ${index + 1}`}
-                width={0}
-                height={0}
-                sizes="(max-width: 1200px) 90vw, (max-width: 1400px) 80vw, (max-width: 1600px) 70vw, 60vw"
-                style={{ width: "100%", height: "auto" }}
-                priority={true}
-                className="mx-auto"
-              />
-            </div>
-          ))}
-        </div>
+        {images.map((image, index) => (
+          <div className="border py-4 border-none" key={index}>
+            <Image
+              src={image}
+              alt={`Image ${index + 1}`}
+              width={0}
+              height={0}
+              sizes="(max-width: 1200px) 90vw, (max-width: 1400px) 80vw, (max-width: 1600px) 70vw, 60vw"
+              style={{ width: "100%", height: "auto" }}
+              priority={true}
+              className="mx-auto"
+            />
+          </div>
+        ))}
       </div>
+
       <ScrollUpButton />
     </div>
   );
