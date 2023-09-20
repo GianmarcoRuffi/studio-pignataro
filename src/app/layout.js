6bf9e097b81d2a35f3c7da9ae0506228e5105d80
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-09-20 17:07:00 +0200

    Aggiornamento descrizione sito

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,49 +22,18 @@ config.autoAddCss = false;
 import Header from "../components/Header";
 // import Footer from "../components/Footer";
 
-//Metadata
-
-export const metadata = {
-  title: "Gianluca Pignataro Architetto",
-  description:
-    "Studio Gianluca Pignataro Architetto - Cagliari. Elenco dei progetti di Gianluca Pignataro, pubblicazioni e articoli, biografia e contatti dello studio.",
-  // openGraph: {
-  //   title: "Architetto Pignataro",
-  //   description: "Architetto Gianluca Pignataro",
-  //   image: "/Bio_2.jpg",
-  // },
-};
-
 //Layout//
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={myFont.className}>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="robots" content="noodp,noydir" />
-        <description>{metadata.description}</description>
-        {metadata.description}
+        <title>Gianluca Pignataro Architetto</title>
+
         <meta
           name="description"
-          content="Studio Gianluca Pignataro Architetto - Cagliari. Elenco dei progetti di Gianluca Pignataro, pubblicazioni e articoli, biografia e contatti dello studio."
+          content="Architetto Gianluca Pignataro, professionista con venti anni di esperienza in grado di ideare con creatività, gestire la complessità di un progetto e realizzarlo con competenza tecnica e senso artistico."
         />
-
-        <meta
-          property="og:description"
-          content="Studio Gianluca Pignataro Architetto - Cagliari. Elenco dei progetti di Gianluca Pignataro, pubblicazioni e articoli, biografia e contatti dello studio."
-        />
-
-        {/* {metadata.openGraph && (
-          <>
-            <meta property="og:title" content={metadata.openGraph.title} />
-            <meta
-              property="og:description"
-              content={metadata.openGraph.description}
-            />
-            <meta property="og:image" content="/Bio_2.jpg" />
-          </>
-        )} */}
       </Head>
 
       <body>
