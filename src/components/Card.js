d7b81a31aba212a4dbb913949ac0716c68b987f9
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-09 17:51:13 +0200

    feature/graphic-overhaul: Updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,20 @@ import React from "react";
 
 export default function Card({ description, imageSource, source, date }) {
   return (
-    <>
-      <div className="rounded overflow-hidden shadow-lg ">
-        <div style={{ display: "flex" }}>
-          <img src={imageSource} alt="" />
-        </div>
-        <div className="presses-box px-6 p-4 ">
-          <div className="font-bold mb-2 break-words text-sm md:text-lg">
-            {description}
-          </div>
-          <div className="mb-2 break-words card-link  text-xs md:text-base">
-            {source}
-          </div>
-          <p className="text-gray-700 text-xs md:text-base">{date}</p>
-        </div>
+    <div className="rounded overflow-hidden shadow-lg">
+      <div style={{ display: "flex" }}>
+        <img src={imageSource} alt="" />
       </div>
-    </>
+      <div className="presses-box px-6 p-4">
+        <div
+          className="font-bold mb-2 break-words text-sm md:text-lg"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        <div className="mb-2 break-words card-link text-xs md:text-base">
+          {source}
+        </div>
+        <p className="text-gray-700 text-xs md:text-base">{date}</p>
+      </div>
+    </div>
   );
 }
