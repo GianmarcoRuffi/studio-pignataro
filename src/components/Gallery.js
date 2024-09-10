26b604e4fa22ae9331055e265a5899e840eb06f8
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-10 12:50:50 +0200

    improvement-1: Updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import ScrollUpButton from "./ScrollUpButton";
+import styles from './gallery.module.css';
 
 export default function Gallery({
   images,
@@ -23,18 +24,32 @@ export default function Gallery({
 
   return (
     <div className="flex-col justify-center bg-gray-100">
-      <div className="mb-20 flex flex-col align-center mx-2 md:ml-16">
-        <h1 className="text-3xl font-semibold py-4">{galleryTitle}</h1>
-        <p className="text-gray-500">{galleryDescription}</p>
-        <div className="gallery-links">{renderGalleryLinks()}</div>
-        <span className="text-gray-500 text-xs">{imgCredits}</span>
+      <div className="flex flex-col md:flex-row md:justify-between mx-2 md:ml-4 md:mr-16 mb-10">
+        <div className="info-container bg-gray-50 p-6 mb-8 md:mb-0 flex-1 md:flex-none md:w-2/3 lg:w-1/2 xl:w-3/6 shadow-lg border border-gray-200 mt-8">
+          <h1 className="text-3xl font-semibold py-4">{galleryTitle}</h1>
+          <p className="text-gray-500">{galleryDescription}</p>
+          <div className="gallery-links">{renderGalleryLinks()}</div>
+          <span className="text-gray-500 text-xs">{imgCredits}</span>
+        </div>
+
+        <div className="button-container flex items-center md:ml-8 mt-8">
+          <a
+            href="/projects"
+            className={styles.buttonLink}
+          >
+            <span>Torna alla galleria dei progetti</span>
+          </a>
+        </div>
       </div>
 
       {/* Modifica da flex a grid per ottenere 2 card per riga */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 bg-gray-100">
+      <div className="photos-container grid grid-cols-1 md:grid-cols-2 gap-8 bg-gray-100 px-4 pt-8 pb-8">
         {images
           ? images.map((image, index) => (
-              <div className="border py-4 border-none" key={index}>
+              <div
+                className="bg-gray-100 p-4 border border-gray-200 shadow-md"
+                key={index}
+              >
                 <Image
                   src={image}
                   alt={`Image ${index + 1}`}
