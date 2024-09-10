3fd4ee957bed1d781db4ca2db3f65f1e4d19b41c
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-10 16:05:07 +0200

    improvement-1: Updated

--- src/app/projects/[slug]/page.js
+++ src/app/projects/[slug]/page.js
@@ -1,29 +1,40 @@
 import { notFound } from "next/navigation";
-import projects from "../../../data/data";
 import Gallery from "../../../components/Gallery";
+import projects from "../../../data/data";
 
 export async function generateStaticParams() {
-  // cicla l'oggetto projects e restituisce lo slug - per ciascuno verrà creata una rotta projects/[slug] - deve chiamarsi esattamente come il segmento di rotta specificato
-  return projects.map((p) => ({
-    slug: p.slug,
+  // Crea i parametri per ogni percorso dinamico
+  const paths = projects.map((project) => ({
+    slug: project.slug,
   }));
+
+  return paths;
 }
 
-// params contiene il risultato ritornato da generateStaticParams
-export default function Page({ params }) {
-  const { slug } = params;
-
-  // prendiamo dall'oggetto projects il progetto giusto tramite slug
+export async function getProjectData(slug) {
+  // Trova il progetto basato sullo slug
   const projectIndex = projects.findIndex((p) => p.slug === slug);
   const project = projects[projectIndex];
-
   // se non esiste -> 404
   if (!project) notFound();
 
-  // Troviamo il progetto precedente e successivo
+  // Trova il progetto precedente e successivo
   const prevProject = projectIndex > 0 ? projects[projectIndex - 1] : null;
   const nextProject =
     projectIndex < projects.length - 1 ? projects[projectIndex + 1] : null;
+
+  return { project, prevProject, nextProject };
+}
+
+export default async function Page({ params }) {
+  const { slug } = params;
+  const data = await getProjectData(slug);
+
+  if (!data) {
+    notFound();
+  }
+
+  const { project, prevProject, nextProject } = data;
 
   return (
     <div>
@@ -33,8 +44,8 @@ export default function Page({ params }) {
         galleryDescription={project.description}
         galleryLinks={project.externalLink}
         imgCredits={project.imgCredits}
-        prevProject={prevProject} // Passiamo il progetto precedente
-        nextProject={nextProject} // Passiamo il progetto successivo
+        prevProject={prevProject}
+        nextProject={nextProject}
       />
     </div>
   );
