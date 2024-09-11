7ab98fbf23a2d4f85ced32dfe9cde7823e0747f6
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-11 12:03:29 +0200

    staging: Fixed Bio page image scalability

--- src/app/bio/page.js
+++ src/app/bio/page.js
@@ -1,17 +1,54 @@
 "use client";
+import { useEffect, useRef } from "react";
 import styles from "./bio.module.css";
 import { bioData } from "../../data/bioData";
 
 export default function Bio() {
+  const imageRef = useRef(null);
+  const bioBoxRef = useRef(null);
+
+  useEffect(() => {
+    const updateBioBoxHeight = () => {
+      if (imageRef.current && bioBoxRef.current) {
+        // Trova l'altezza dell'immagine e applica al bio-box
+        const img = imageRef.current.querySelector("img");
+        if (img) {
+          bioBoxRef.current.style.height = `${img.offsetHeight}px`;
+        }
+      }
+    };
+
+    // Aggiorna l'altezza al caricamento dell'immagine
+    const img = imageRef.current.querySelector("img");
+    if (img) {
+      img.addEventListener("load", updateBioBoxHeight);
+      // Esegui anche al montaggio
+      updateBioBoxHeight();
+    }
+
+    // Pulisci l'evento listener
+    return () => {
+      if (img) {
+        img.removeEventListener("load", updateBioBoxHeight);
+      }
+    };
+  }, []);
+
   return (
     <div
       className={`${styles["bio-container"]} flex bg-gray-100 max-lg:flex-col`}
     >
-      <div className={`${styles["image-container"]} lg:w-2/3 lg:mr-4`}>
+      <div
+        className={`${styles["image-container"]} lg:w-2/3 lg:mr-4`}
+        ref={imageRef}
+      >
         <img src="/Bio.jpg" alt="bio" className="" />
       </div>
 
-      <div className={`${styles["bio-box"]} lg:w-1/3 text-justify py-4`}>
+      <div
+        className={`${styles["bio-box"]} lg:w-1/3 text-justify py-4`}
+        ref={bioBoxRef}
+      >
         <h1>
           <strong>{bioData.name}</strong>
         </h1>
