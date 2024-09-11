f7a78b1cfc3fe358c2a8a47d6cb3a84823deb64c
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-11 09:21:55 +0200

    staging: Updated vh in not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import styles from "./not-found.module.css";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Siamo spiacenti, la pagina che hai cercato non esiste.</h2>
-      <Link href="/">
+    <div className={styles.notFoundContainer}>
+      <h2 className={styles.title}>Siamo spiacenti, la pagina che hai cercato non esiste.</h2>
+      <Link href="/" className={styles.link}>
         <strong>Clicca qui per tornare alla home</strong>
       </Link>
     </div>
