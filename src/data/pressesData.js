ae49ccc0d01d6f35ace9ba574986f5303e112b31
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-12 11:24:15 +0200

    staging: aggiornati nomi dei file delle immagini

--- src/data/pressesData.js
+++ src/data/pressesData.js
@@ -2,7 +2,7 @@ const pressesData = [
   {
     description:
       "“Gianluca Pignataro – 30 anni di inconsuetudine”<br />Monografia a cura di Luigi Corda – “Associazione Culturale Timeless”",
-    imageSource: "/Pubblicazioni/Inconsuetudine_cover.jpg",
+    imageSource: "/Pubblicazioni/TIMELESS_cover.jpg",
     source: "",
     date: "Dicembre 2023",
   },
@@ -17,7 +17,7 @@ const pressesData = [
   {
     description:
       "“15 motivi (…)”, articolo pubblicato nella rivista online “HOMIFY”",
-    imageSource: "/Pubblicazioni/HOMIFY_cover1.jpg",
+    imageSource: "/Pubblicazioni/HOMIFY_cover_1.jpg",
     source:
       "https://www.homify.it/librodelleidee/7792268/15-motivi-per-scegliere-il-cocciopesto-e-i-laterizi-per-i-rivestimenti",
     date: "Febbraio 2021",
@@ -25,7 +25,7 @@ const pressesData = [
   {
     description:
       "Recupero dei sottotetti (…)”, articolo pubblicato nella rivista online “HOMIFY”",
-    imageSource: "/Pubblicazioni/HOMIFY_cover2.jpg",
+    imageSource: "/Pubblicazioni/HOMIFY_cover_2.jpg",
     source:
       "https://www.homify.it/librodelleidee/6101558/recupero-dei-sottotetti-misure-e-normative-per-il-piemonte",
     date: "Novembre 2018",
@@ -33,7 +33,7 @@ const pressesData = [
   {
     description:
       "Un appartamento moderno rivoluzionato”, articolo pubblicato nella rivista online “HOMIFY”",
-    imageSource: "/Pubblicazioni/HOMIFY_cover3.jpg",
+    imageSource: "/Pubblicazioni/HOMIFY_cover_3.jpg",
     source:
       "https://www.homify.it/librodelleidee/778802/un-appartamento-moderno-rivoluzionario",
     date: "Maggio 2016",
@@ -41,21 +41,21 @@ const pressesData = [
   {
     description:
       "“Sull’onda del gres”, articolo pubblicato nella rivista “YOUTRADE Casa”",
-    imageSource: "/Pubblicazioni/GRES_cover_resized.jpg",
+    imageSource: "/Pubblicazioni/YOUTRADE_cover_2.jpg",
     source: "",
     date: "Settembre 2015",
   },
   {
     description:
       "“Diamoci un taglio (ma diagonale)”, copertina e articolo pubblicato nella rivista “YOUTRADE Casa”",
-    imageSource: "/Pubblicazioni/YOUTRADE.jpg",
+    imageSource: "/Pubblicazioni/YOUTRADE_cover_1.jpg",
     source: "",
     date: "Maggio 2015",
   },
   {
     description:
       "“Sulle orme della Storia”, articolo pubblicato nella rivista “Brava Casa”",
-    imageSource: "/Pubblicazioni/Bravacasa.jpg",
+    imageSource: "/Pubblicazioni/BRAVACASA_cover.jpg",
     source: "",
     date: "Marzo 1999",
   },
