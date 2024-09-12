5a5add71bb8fe856d2844f79b2353e563f079b77
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-12 13:07:03 +0200

    staging: Updated transitions

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,33 +1,35 @@
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import projects from "../../data/data";
 import ProjectCard from "../../components/ProjectCard";
 import ScrollUpButton from "../../components/ScrollUpButton";
-
-function renderProjects() {
-  return projects.map((project, index) =>
-    project.invisible ? (
-      ""
-    ) : (
-      <Link key={index} href={`/projects/${project.slug}`}>
-        <ProjectCard
-          name={project.projectName}
-          imageSource={project.imgSrc}
-          description={project.description}
-          className="uniform-card"
-        ></ProjectCard>
-      </Link>
-    )
-  );
-}
+import styles from "./projects.module.css";
 
 export default function Projects() {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="projects-container flex-row justify-center">
-      {/* Griglia per le card */}
-      <div className="card-box bg-gray-100 p-10 grid grid-cols-1 md:grid-cols-2 gap-8">
-        {renderProjects()}
+      <div
+        className={`${styles.cardBox} ${loaded ? 'loaded' : ''} bg-gray-100 p-10 grid grid-cols-1 md:grid-cols-2 gap-8`}
+        onLoad={() => setLoaded(true)} // Mark cards as loaded when images are loaded
+      >
+        {projects.map((project, index) =>
+          project.invisible ? null : (
+            <Link key={index} href={`/projects/${project.slug}`}>
+              <div className="project-card">
+                <ProjectCard
+                  name={project.projectName}
+                  imageSource={project.imgSrc}
+                  description={project.description}
+                />
+              </div>
+            </Link>
+          )
+        )}
       </div>
-      <ScrollUpButton></ScrollUpButton>
+      <ScrollUpButton />
     </div>
   );
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,24 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import styles from "./project-card.module.css"; // Importa il modulo CSS
 
 export default function ProjectCard({ name, imageSource, description }) {
+  const [loaded, setLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoaded(true);
+  };
+
   return (
     <div className="rounded overflow-hidden shadow-lg lg:h-[600px] md:h-[500px] sm:h-auto flex flex-col transition-opacity duration-300 ease-in-out hover:opacity-75">
-      <div className="lg:h-4/5 sm:h-auto md:h-3/5 w-full">
-        {/* Imposta altezza e larghezza fissa per l'immagine */}
-        <img
+      <div className="lg:h-4/5 sm:h-auto md:h-3/5 w-full relative">
+        <Image
           src={imageSource}
           alt={name}
-          className="w-full h-full object-cover"
+          layout="fill"
+          objectFit="cover"
+          className={`${styles.image} ${loaded ? styles.loaded : ''}`}
+          onLoad={handleImageLoad}
         />
+        <div className="absolute inset-0 bg-transparent transition-opacity duration-300 ease-in-out" />
       </div>
 
       <div className="px-6 py-4 flex-1 flex flex-col justify-center">
-        {/* Nome del progetto */}
         <div className="font-bold text-lg mb-2 text-left break-words text-sm md:text-base">
           {name}
         </div>
-
-        {/* Descrizione limitata a 3 righe */}
         <p className="text-gray-700 text-sm md:text-base line-clamp-3 text-left">
           {description}
         </p>
