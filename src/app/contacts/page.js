adce1009e7fc293b740a994f9598964dcef3735a
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-13 11:21:14 +0200

    staging: aggiunta transizione contatti

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,11 +1,30 @@
 "use client";
+import { useState, useEffect } from "react";
 import { contactsData } from "../../data/contactsData";
 
 export default function Contacts() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  // Verifica se l'immagine è stata già caricata
+  useEffect(() => {
+    const img = document.querySelector("img[src='/Studio.jpg']");
+    if (img && img.complete) {
+      setIsImageLoaded(true); // Se già caricata, attiva subito l'immagine
+    }
+  }, []);
+
   return (
     <div className="contacts-container flex justify-center items-center bg-gray-100 max-lg:flex-col">
-      <div className="image-container lg:w-2/3 lg:pr-4 ">
-        <img src="/Studio.jpg" alt="studio" className="max-w-full" />
+      <div className="image-container lg:w-2/3 lg:pr-4">
+        {/* Aggiunta di opacity e transizione sull'immagine */}
+        <img
+          src="/Studio.jpg"
+          alt="studio"
+          className={`max-w-full transition-opacity duration-700 ${
+            isImageLoaded ? "opacity-100" : "opacity-0"
+          }`}
+          onLoad={() => setIsImageLoaded(true)}
+        />
       </div>
 
       <div className="contacts-box py-8 lg:w-1/3 flex items-center text-xs md:text-sm">
