5ce9b12ecb8ec91fef57c0d3cfb5e30a7fa58462
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-17 12:21:34 +0200

    rework: Updated

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -11,20 +11,19 @@ export default function ProjectCard({ name, imageSource, description }) {
   };
 
   return (
-    <div className="rounded overflow-hidden shadow-lg lg:h-[600px] md:h-[500px] h-auto flex flex-col transition-opacity duration-1000 ease-in-out hover:opacity-75">
+    <div className="rounded overflow-hidden shadow-2xl lg:h-[600px] md:h-[500px] h-auto flex flex-col transition-opacity duration-1000 ease-in-out hover:opacity-75">
       <div className="h-[300px] lg:h-4/5 md:h-3/5 w-full relative">
         <Image
           src={imageSource}
           alt={name}
           layout="fill"
-          objectFit="cover"
-          className={`${styles.image} ${loaded ? styles.loaded : ""}`}
+          className={`${styles.image} ${loaded ? styles.loaded : ""} object-cover xs:object-cover sm:object-cover md:object-cover lg:object-contain xl:object-contain`}
           onLoad={handleImageLoad}
         />
         <div className="absolute inset-0 bg-transparent transition-opacity duration-1000 ease-in-out" />
       </div>
 
-      <div className="px-6 py-4 flex-1 flex flex-col justify-center">
+      <div className="px-6 py-4 flex-1 flex flex-col justify-center items-center">
         <div className="font-bold text-lg mb-2 text-left break-words text-sm md:text-base">
           {name}
         </div>
