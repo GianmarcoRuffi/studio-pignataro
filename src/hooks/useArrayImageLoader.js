ef7cbb0e4262bfe0a836775f737270110a6eb3e7
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-24 17:20:01 +0200

    Revert "staging: Updated transitions"
    This reverts commit bde1603d10a0c1d0164230f53990c04922ac0a2a.

--- src/hooks/useArrayImageLoader.js
+++ src/hooks/useArrayImageLoader.js
@@ -1,29 +1,22 @@
 import { useState, useEffect } from "react";
 
 export function useArrayImageLoader(images) {
-  const [transitionClass, settransitionClass] = useState(
-    images.map(() => "opacity-0")
-  );
+  const [areImagesLoaded, setAreImagesLoaded] = useState(false);
 
   useEffect(() => {
-    const imagePromises = images.map((src, index) => {
+    const imagePromises = images.map((src) => {
       return new Promise((resolve) => {
         const img = new Image();
         img.src = src;
-        img.onload = () => {
-          settransitionClass((prev) => {
-            const newClasses = [...prev];
-            newClasses[index] = "transition-opacity duration-700 opacity-100";
-            return newClasses;
-          });
-          resolve();
-        };
+        img.onload = resolve;
         img.onerror = resolve;
       });
     });
 
-    Promise.all(imagePromises).then(() => {});
+    Promise.all(imagePromises).then(() => {
+      setAreImagesLoaded(true);
+    });
   }, [images]);
 
-  return transitionClass;
+  return areImagesLoaded;
 }
