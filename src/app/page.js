603b0648270be0150a0604d9b48ffa496cfb43ab
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-09-25 10:48:35 +0200

    staging: Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,46 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "../components/Slider/Slider";
 import projects from "../data/data";
 import styles from "./styles/page.home.module.css";
 import MainSkeleton from "../components/MainSkeleton/MainSkeleton";
-import SplashScreen from "../components/SplashScreen/SplashScreen";
 import { useArrayImageLoader } from "../hooks/useArrayImageLoader";
 
 export default function Home() {
   const projectImages = projects.map((project) => project.image);
+  const [isRendered, setIsRendered] = useState(false);
+  const containerRef = useRef(null);
 
   const areImagesLoaded = useArrayImageLoader(projectImages);
 
-  return (
-    <div className={styles.homeContainer}>
+  useEffect(() => {
+    if (areImagesLoaded) {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === "childList" || mutation.type === "attributes") {
+            setIsRendered(true);
+          }
+        });
+      });
 
-      {areImagesLoaded ? <Slider projects={projects} /> : <MainSkeleton />}
+      if (containerRef.current) {
+        observer.observe(containerRef.current, {
+          childList: true,
+          attributes: true,
+        });
+      }
+
+      return () => {
+        if (containerRef.current) {
+          observer.disconnect();
+        }
+      };
+    }
+  }, [areImagesLoaded]);
+
+  return (
+    <div className={styles.homeContainer} ref={containerRef}>
+      {isRendered ? <Slider projects={projects} /> : <MainSkeleton />}
     </div>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,17 +3,15 @@ import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
-import { useArrayImageLoader } from "../../hooks/useArrayImageLoader";
 import styles from "./slider.module.css";
 
 function Slider({ projects }) {
   const [activeIndex, setActiveIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const [isRendered, setIsRendered] = useState(false);
   const slideContainerRef = useRef(null);
-
-  const imageSources = projects.map((project) => project.imgSrc);
-  const areImagesLoaded = useArrayImageLoader(imageSources);
+  const imageRefs = useRef([]);
+  let imagesRendered = useRef(0); // Track how many images are rendered
 
   const handleResize = () => {
     setIsLargeScreen(window.matchMedia("(min-width: 1024px)").matches);
@@ -27,6 +25,12 @@ function Slider({ projects }) {
     };
   }, []);
 
+  useEffect(() => {
+    if (imagesRendered.current === projects.length) {
+      setIsRendered(true); // All images have rendered
+    }
+  }, [imagesRendered.current]);
+
   const nextSlide = () => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % projects.length);
   };
@@ -39,23 +43,15 @@ function Slider({ projects }) {
 
   useEffect(() => {
     let interval;
-
-    if (!isHovered) {
+    if (!isHovered && isRendered) {
       interval = setInterval(() => {
         nextSlide();
       }, 3000);
     }
-
     return () => {
       clearInterval(interval);
     };
-  }, [isHovered, activeIndex]);
-
-  useEffect(() => {
-    if (slideContainerRef.current !== null) {
-      slideContainerRef.current.style.height = `${slideContainerRef.current.scrollHeight}px`;
-    }
-  }, [activeIndex, projects]);
+  }, [isHovered, isRendered]);
 
   return (
     <div
@@ -81,17 +77,19 @@ function Slider({ projects }) {
               <a href={`/projects/${project.slug}`} rel="noopener noreferrer">
                 <div className={styles.imageWrapper}>
                   <Image
+                    ref={(el) => (imageRefs.current[index] = el)} // Store reference to image
                     src={project.imgSrc}
                     alt={`Slide ${index}`}
                     sizes="(max-width: 1200px) 90vw, (max-width: 1400px) 80vw, (max-width: 1800px) 70vw, 60vw"
                     style={{
                       objectFit: isLargeScreen ? "contain" : "cover",
-                      opacity: areImagesLoaded ? 1 : 0,
+                      opacity: isRendered ? 1 : 0,
                       transition: "opacity 0.5s ease",
                     }}
                     priority={true}
                     fill={true}
                     onLoadingComplete={() => {
+                      imagesRendered.current += 1;
                       if (slideContainerRef.current) {
                         slideContainerRef.current.style.height = `${slideContainerRef.current.scrollHeight}px`;
                       }
