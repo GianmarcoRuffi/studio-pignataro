3a6da81deb9bd6594b14d6bac88a0172b87b01ca
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-10-03 10:47:33 +0200

    staging: Fixed bio page

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,15 +1,48 @@
 "use client";
+
+import { useEffect, useRef, useState } from "react";
 import { useImageLoader } from "../../hooks/useImageLoader";
 import styles from "./bio.module.scss";
 import { bioData } from "../../data/bioData";
 
 export default function Bio() {
   const isImageLoaded = useImageLoader(`img[src='${bioData.image}']`);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const bioBoxRef = useRef<HTMLDivElement | null>(null);
+  const [imageHeight, setImageHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+
+      if (window.innerWidth >= 1024 && isImageLoaded && imageRef.current) {
+        const height = imageRef.current.offsetHeight;
+        setImageHeight(height);
+        if (bioBoxRef.current) {
+          bioBoxRef.current.style.height = `${height}px`;
+        }
+      } else {
+
+        if (bioBoxRef.current) {
+          bioBoxRef.current.style.height = "auto";
+        }
+      }
+    };
+
+
+    window.addEventListener("resize", handleResize);
+
+
+    handleResize();
+
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isImageLoaded]);
 
   return (
     <div className={`${styles["bio-container"]} flex max-lg:flex-col`}>
-      <div className={`${styles["image-box"]} lg:w-2/3`}>
+      <div className={`${styles["image-box"]} lg:w-1/2`}>
         <img
+          ref={imageRef}
           src={bioData.image}
           alt="bio"
           className={`transition-opacity duration-700 ${
@@ -19,7 +52,9 @@ export default function Bio() {
       </div>
 
       <div
-        className={`${styles["bio-box"]} lg:w-1/3 text-justify py-4 lg:ml-4`}
+        ref={bioBoxRef}
+        className={`${styles["bio-box"]} lg:w-1/2 text-justify py-4 lg:ml-4`}
+        style={{ height: imageHeight ? `${imageHeight}px` : "auto" }}
       >
         <h1>
           <strong>{bioData.name}</strong>
